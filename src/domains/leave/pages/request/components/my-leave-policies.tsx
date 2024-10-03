a040381ad7e42e8b75bac47126126dd1efff77bb
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2024-10-03 13:23:19 +0545

    refactor rendering logic

--- src/domains/leave/pages/request/components/my-leave-policies.tsx
+++ src/domains/leave/pages/request/components/my-leave-policies.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Grid2 } from '@mui/material';
 import { LeaveDetail } from '@/domains/leave/components';
 import { useLeaveRequest } from '../context/leave-request-provider';
@@ -5,23 +6,24 @@ import { useLeaveRequest } from '../context/leave-request-provider';
 export const MyLeavePolicies = () => {
   const { isLoading, isError, errorMessage, myLeavePolicies } = useLeaveRequest();
 
+  let content: React.ReactNode | null = null;
   if (isLoading) {
-    return <>loading...</>;
-  }
-  if (isError) {
-    return <>{errorMessage}</>;
-  }
-  if (myLeavePolicies.length <= 0 && !isLoading) {
-    return <>You are not assigned to any leave policies.</>;
+    content = <>loading...</>;
+  } else if (isError) {
+    content = <>{errorMessage}</>;
+  } else if (myLeavePolicies.length <= 0 && !isLoading) {
+    content = <>You are not assigned to any leave policies.</>;
+  } else {
+    content = myLeavePolicies.map((leave) => (
+      <Grid2 size={{ xs: 12, md: 4 }} key={leave.id}>
+        <LeaveDetail key={leave.id} {...leave} />
+      </Grid2>
+    ));
   }
 
   return (
     <Grid2 container columnSpacing={2} rowSpacing={2}>
-      {myLeavePolicies.map((leave) => (
-        <Grid2 size={{ xs: 12, md: 4 }} key={leave.id}>
-          <LeaveDetail key={leave.id} {...leave} />
-        </Grid2>
-      ))}
+      {content}
     </Grid2>
   );
 };
