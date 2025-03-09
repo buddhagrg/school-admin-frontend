da557e56ea6b9a9e038f01a0619296ad110cb59a
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2025-03-09 20:25:55 +0545

    use appropriate type

--- src/app/staff/types/staff-type.ts
+++ src/app/staff/types/staff-type.ts
@@ -5,13 +5,13 @@ import {
   ParentsInfoSchema,
   StaffFormSchema
 } from './staff-schema';
-import { UserAccountBasic } from '@/app/manage-users/types';
+import { NameIdType } from '@/types';
 
 export type StaffFormProps = z.infer<typeof StaffFormSchema>;
 export type AdminStaffProps = z.infer<typeof AdminStaffSchema>;
 export type StaffFormPropsWithId = StaffFormProps & { id: number };
 export type StaffData = {
-  staff: UserAccountBasic[];
+  staff: NameIdType[];
 };
 export type ParentsInfo = z.infer<typeof ParentsInfoSchema>;
 export type BasicInfo = z.infer<typeof BasicInfoSchema>;
