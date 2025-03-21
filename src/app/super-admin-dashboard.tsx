92210cab2b0b01fe9fd27aba5fb1de13ff152922
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2025-03-21 18:40:03 +0545

    clearing unused variables

--- src/app/super-admin-dashboard.tsx
+++ src/app/super-admin-dashboard.tsx
@@ -1,11 +1,3 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { getUserMenus } from './auth/auth-slice';
-
 export const SuperAdminDashboard = () => {
-  const menus = useSelector(getUserMenus);
-  console.log(menus);
-
-  useEffect(() => {}, []);
   return <>super admin area</>;
 };
